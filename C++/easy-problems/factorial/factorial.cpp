7e99ca2f774ba63b566a1c83695c11ac204d9e70
Walter Goodzey <145727171+WalterGoodzey@users.noreply.github.com>
2024-11-12 11:20:01 -0600

    added factorial.cpp

---
+++ C++/easy-problems/factorial/factorial.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+// Iterative implementation
+int factorial(int n) {
+    int result;
+
+    // Implement function
+
+    return result;
+}
+
+// Recursive implementation
+int factorialR(int n) {
+    
+    // Implement function
+
+    return 1; // change this
+}
+
+int main() {
+    int i;
+    for (i = 0; i <= 12; i++) {
+        std::cout << i << ": I: " << factorial(i) << " R: "<< factorialR(i) << std::endl;
+    }
+    return 0;
+}

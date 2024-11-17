292e8b0fee492e052638aed76221e43bd48c81bb
Walter Goodzey <145727171+WalterGoodzey@users.noreply.github.com>
2024-11-16 21:45:24 -0600

    added some stuff to intentionally make bad for valgrind :)

---
+++ C++/medium-problems/memory-managment/valgrind-practice/valgrind-practice.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+struct DataHolder {
+	int val;
+
+	DataHolder(int val) : val(val) {}
+	~DataHolder() {
+		std::cout << "DataHolder with value " << val << " is being destroyed" << std::endl;
+	}
+};
+
+void UseCMemoryAllocation() {
+	std::cout << "Using C-style memory allocation (malloc and free):" << std::endl;
+
+	int* array = (int*)malloc(5 * sizeof(int));
+	
+	if (array == nullptr) {
+		std::cerr << "Memory allocation failed." << std::endl;
+		return;
+	}
+
+	for (int i = 0; i < 5; ++i) {
+		array[i] = i + 10;
+		std::cout << "array[" << i << "} = " << array[i] << std::endl;
+	}
+
+	free(array);
+	std::cout << "Memory allocated with malloc has been freed." <<std::endl;
+}
+
+void UseCPPMemoryAllocation() {
+	std::cout << "\nUsing C++-style memory allocation (new and delete):" << std::endl;
+
+	DataHolder* data_ptr = new DataHolder(2);
+
+	std::cout << "DataHolder value: " << data_ptr -> val << std::endl;
+
+	delete data_ptr;
+	std::cout << "Memory allocated with new has been deleted." << std::endl;
+
+	DataHolder* data_array = new DataHolder[3]{DataHolder(1), DataHolder(2), DataHolder(3)};
+
+	for (int i = 0; i < 3; ++i) {
+		std::cout << "DataHolder array[" << i << "] value: " << data_array[i].val << std::endl;
+	}
+
+	delete[] data_array;
+	std::cout << "Array allocated with new[] has been deleted." << std::endl;
+}
+
+int main() {
+	UseCMemoryAllocation();
+	UseCPPMemoryAllocation();
+	return 0;
+}
